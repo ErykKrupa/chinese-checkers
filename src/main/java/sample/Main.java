69d208dc9bd5609e89612381cee748bc0449a476
Jakub Dochniak <jakub.dochniak@protonmail.com>
2018-12-03 16:29:27 +0100

    Add first client window

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -10,12 +10,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("/Sample.fxml"));
+        primaryStage.setTitle("Chinese checkers - board game");
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);

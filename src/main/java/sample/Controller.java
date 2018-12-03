0cf7e6591d22130407a286b07c3da73104f6b7c9
Jakub Dochniak <jakub.dochniak@protonmail.com>
2018-12-03 20:37:00 +0100

    Add single player game btn and multiplayer game setting window

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,4 +1,31 @@
 package sample;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static java.lang.System.exit;
+
 public class Controller {
+
+    public void newMultiplayerGameBtnClick() throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/newGamePopup.fxml"));
+        Stage newGamePopupStage = new Stage();
+        newGamePopupStage.setTitle("Game settings");
+        newGamePopupStage.setScene(new Scene(root, 300, 400));
+        newGamePopupStage.show();
+    }
+
+    public void joinGameBtnClick(){
+        //TODO send join existing game request to server
+    }
+
+    public void startSinglePlayerGameBtnClick(){
+        //TODO
+    }
+
+    public void exitBtnClick(){
+        exit(0);
+    }
 }

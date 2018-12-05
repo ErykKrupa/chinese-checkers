e05310bfebfbe1ed58dc40e8d96f1719219f430e
Jakub Dochniak <jakub.dochniak@protonmail.com>
2018-12-05 19:57:24 +0100

    Add server connection function in client

--- src/main/java/dochniak_krupa/client/Client.java
+++ src/main/java/dochniak_krupa/client/Client.java
@@ -6,6 +6,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.*;
+import java.net.Socket;
+import java.nio.Buffer;
+
 public class Client extends Application {
 
     static Stage menuStage = new Stage();
@@ -16,6 +20,12 @@ public class Client extends Application {
         menuStage.setTitle("Chinese Checkers - Menu");
         menuStage.setResizable(false);
         menuStage.show();
+    }
+
+    public void connectToServer() throws IOException{
+        Socket socket = new Socket("",9090);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
     }
 
     public static void main(String[] args) {
